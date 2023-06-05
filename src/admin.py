0139aedb28477e2208163f216885c3ad0850e515
Antonio Gamero <125345806+nono343@users.noreply.github.com>
2023-06-05 18:27:31 +0000

    first

--- src/admin.py
+++ src/admin.py
@@ -1,6 +1,14 @@
 import os
 from flask_admin import Admin
 from models import db, User
+from models import db, Character
+from models import db, Starships
+from models import db, Homeworld
+from models import db, FavsCharacter
+from models import db, FavsHomeworld
+from models import db, FavsStarships
+
+
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -8,9 +16,37 @@ def setup_admin(app):
     app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
     admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
 
+    class CharacterAdmin(ModelView):
+        column_list = ("id", "name", "birth_year", "eye_color", "gender", "hair_color", "height", "films", "homeworld_id", "mass", "skin_color", "species", "starships_id", "url", "vehicles")
+        form_columns = ("name", "birth_year", "eye_color", "gender", "hair_color", "height", "films", "homeworld_id", "mass", "skin_color", "species", "starships_id", "url", "vehicles")
+        column_hide_backrefs = False
+    
+    class FavsCharacterAdmin(ModelView):
+        column_list = ("id", "user_id", "character_id")
+        form_columns = ("user_id", "character_id")
+        column_hide_backrefs = False
+
+    class FavsStarshipsAdmin(ModelView):
+        column_list = ("id", "user_id", "starships_id")
+        form_columns = ("user_id", "starships_id")
+        column_hide_backrefs = False
+    
+    class FavsHomeworldAdmin(ModelView):
+        column_list = ("id", "user_id", "homeworld_id")
+        form_columns = ("user_id", "homeworld_id")
+        column_hide_backrefs = False
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
+    admin.add_view(CharacterAdmin(Character, db.session))
+    admin.add_view(ModelView(Starships, db.session))
+    admin.add_view(ModelView(Homeworld, db.session))
+    admin.add_view(FavsCharacterAdmin(FavsCharacter, db.session))
+    admin.add_view(FavsHomeworldAdmin(FavsHomeworld, db.session))
+    admin.add_view(FavsStarshipsAdmin(FavsStarships, db.session))
+
+
+
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/app.py
+++ src/app.py
@@ -9,14 +9,21 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
-#from models import Person
+from models import Character
+from models import Homeworld
+from models import Starships
+from models import FavsCharacter
+from models import FavsHomeworld
+from models import FavsStarships
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
+        "postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -27,23 +34,190 @@ CORS(app)
 setup_admin(app)
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+# Endpoint para obtener la información de un todos los character
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
+
+@app.route('/character', methods=['GET'])
+def get_all_characters():
+    characters = Character.query.all()
+    serialized_characters = [character.serialize() for character in characters]
+    return jsonify(serialized_characters)
+
+# Endpoint para obtener la información de un solo character
+
+
+@app.route('/character/<int:character_id>', methods=['GET'])
+def get_character(character_id):
+    character = Character.query.get(character_id)
+    if character is None:
+        return jsonify({'mensaje': 'Personaje no encontrado'}), 404
+    serialized_character = character.serialize()
+    return jsonify(serialized_character)
+
+# Endpoint para listar todos los registros de homeworld
+
+
+@app.route('/homeworld', methods=['GET'])
+def get_all_homeworlds():
+    homeworlds = Homeworld.query.all()
+    serialized_homeworlds = [homeworld.serialize() for homeworld in homeworlds]
+    return jsonify(serialized_homeworlds)
+
+# Endpoint para obtener la información de un solo homeworld
+
+
+@app.route('/homeworld/<int:homeworld_id>', methods=['GET'])
+def get_homeworld(homeworld_id):
+    homeworld = Homeworld.query.get(homeworld_id)
+    if homeworld is None:
+        return jsonify({'mensaje': 'Planeta no encontrado'}), 404
+    serialized_homeworld = homeworld.serialize()
+    return jsonify(serialized_homeworld)
+
+# Endpoint para listar todos los registros de starships
+
+
+@app.route('/starships', methods=['GET'])
+def get_all_starships():
+    starships = Starships.query.all()
+    serialized_starships = [starship.serialize() for starship in starships]
+    return jsonify(serialized_starships)
+
+# Endpoint para obtener la información de una sola starship
+
+
+@app.route('/starships/<int:starship_id>', methods=['GET'])
+def get_starship(starship_id):
+    starship = Starships.query.get(starship_id)
+    if starship is None:
+        return jsonify({'mensaje': 'Nave no encontrada'}), 404
+    serialized_starship = starship.serialize()
+    return jsonify(serialized_starship)
+
+# Endpoint para listar todos los usuarios del blog
+
+
+@app.route('/users', methods=['GET'])
+def get_all_users():
+    users = User.query.all()
+    serialized_users = [user.serialize() for user in users]
+    return jsonify(serialized_users)
+
+# Endpoint para listar todos los favoritos de un usuario
+
+
+@app.route('/users/<int:user_id>/favorites', methods=['GET'])
+def get_all__user_favorites(user_id):
+    character_favorites = FavsCharacter.query.filter_by(user_id=user_id).all()
+    homeworld_favorites = FavsHomeworld.query.filter_by(user_id=user_id).all()
+    starship_favorites = FavsStarships.query.filter_by(user_id=user_id).all()
+
+    serialized_favorites = {
+        'characters': [fav.serialize() for fav in character_favorites],
+        'homeworlds': [fav.serialize() for fav in homeworld_favorites],
+        'starships': [fav.serialize() for fav in starship_favorites]
     }
+    return jsonify(serialized_favorites)
 
-    return jsonify(response_body), 200
+
+@app.route('/users/<int:user_id>/homeworld/<int:homeworld_id>', methods=['POST'])
+def add_favorite_homeworld(user_id, homeworld_id):
+    # Verificar si el usuario existe en la base de datos
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({'mensaje': 'Usuario no encontrado'}), 404
+
+    # Verificar si el homeworld existe en la base de datos
+    homeworld = Homeworld.query.get(homeworld_id)
+    if not homeworld:
+        return jsonify({'mensaje': 'Planeta no encontrado'}), 404
+
+    # Crear un nuevo homeworld favorito para el usuario actual
+    favorite_homeworld = FavsHomeworld(
+        user_id=user_id, homeworld_id=homeworld_id)
+    db.session.add(favorite_homeworld)
+    db.session.commit()
+
+    return jsonify({'mensaje': 'Planeta favorito agregado'}), 200
+
+
+@app.route('/users/<int:user_id>/character/<int:character_id>', methods=['POST'])
+def add_favorite_character(user_id, character_id):
+    # Verificar si el usuario existe en la base de datos
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({'mensaje': 'Usuario no encontrado'}), 404
+
+    # Verificar si el character existe en la base de datos
+    character = Character.query.get(character_id)
+    if not character:
+        return jsonify({'mensaje': 'Personaje no encontrado'}), 404
+
+    # Crear un nuevo character favorito para el usuario actual
+    favorite_character = FavsCharacter(
+        user_id=user_id, character_id=character_id)
+    db.session.add(favorite_character)
+    db.session.commit()
+
+    return jsonify({'mensaje': 'Personaje favorito agregado'}), 200
+
+
+@app.route('/users/<int:user_id>/starships/<int:starships_id>', methods=['POST'])
+def add_favorite_starships(user_id, starships_id):
+    # Verificar si el usuario existe en la base de datos
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({'mensaje': 'Usuario no encontrado'}), 404
+
+    # Verificar si el starships existe en la base de datos
+    starships = Starships.query.get(starships_id)
+    if not starships:
+        return jsonify({'mensaje': 'Nave no encontrada'}), 404
+
+    # Crear un nuevo starships favorito para el usuario actual
+    favorite_starships = FavsStarships(
+        user_id=user_id, starships_id=starships_id)
+    db.session.add(favorite_starships)
+    db.session.commit()
+
+    return jsonify({'mensaje': 'Nave favorita agregada'}), 200
+
+@app.route('/favorite/homeworld/<int:homeworld_id>', methods=['DELETE'])
+def delete_favorite_homeworld(homeworld_id):
+    favorite_homeworld = FavsHomeworld.query.get(homeworld_id)
+    if favorite_homeworld is None:
+        return jsonify({'mensaje': 'Planeta favorito no encontrado'}), 404
+
+    db.session.delete(favorite_homeworld)
+    db.session.commit()
+
+    return jsonify({'mensaje': 'Planeta favorito eliminado'}), 200
+
+
+@app.route('/favorite/character/<int:character_id>', methods=['DELETE'])
+def delete_favorite_character(character_id):
+    favorite_character = FavsCharacter.query.get(character_id)
+    if favorite_character is None:
+        return jsonify({'mensaje': 'Personaje favorito no encontrado'}), 404
+
+    db.session.delete(favorite_character)
+    db.session.commit()
+
+    return jsonify({'mensaje': 'Personaje favorito eliminado'}), 200
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
